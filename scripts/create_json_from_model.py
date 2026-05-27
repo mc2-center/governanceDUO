ad3b8f8708170bd6f24bc1a630d40fb6d889c700
Orion Banks <49208907+Bankso@users.noreply.github.com>
2026-05-27 13:13:36 -0700

    Add script to generate JSON via curator

---
+++ scripts/create_json_from_model.py
@@ -0,0 +1,21 @@
+from synapseclient import Synapse
+from synapseclient.extensions.curator import (
+    bind_jsonschema,
+    generate_jsonschema,
+    register_jsonschema,
+)
+import sys
+
+DATA_MODEL_SOURCE = "sage-ar.model.csv"
+DATA_TYPE = sys.argv[1:] if len(sys.argv) > 1 else None
+OUTPUT_DIRECTORY = "sage-ar-model"
+
+syn = Synapse()
+syn.login()
+
+schemas, file_paths = generate_jsonschema(
+    data_model_source=DATA_MODEL_SOURCE,
+    output=OUTPUT_DIRECTORY,
+    data_types=DATA_TYPE,
+    synapse_client=syn,
+)
